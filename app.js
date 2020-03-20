4b5d7257b8976c6fb0385c5a0792562fbc78c108
rhuanhuan <1210098674@qq.com>
2020-03-20 17:16:11 +0800

    add post method

--- app.js
+++ app.js
@@ -2,30 +2,49 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
+var fileName = path.resolve(__dirname, 'data.txt');
+
 let normalRead = function (res) {
     fs.readFile(fileName, function (err, data) {
         res.end(data);
     });
 };
 
-let streamRead = function (fileName, res) {
-    var length = 0;
+let streamRead = function (req, res) {
+    switch (req.method) {
+        case 'GET': // 处理 GET 方法
+            const stream = fs.createReadStream(fileName);
 
-    var stream = fs.createReadStream(fileName);
-    stream.on('data', function (chunk) {
-        length += chunk.toString().length
-    });
-    stream.on('end', function () {
-        console.log(length)
-    });
+            var length = 0;
+            stream.on('data', (chunk) => {
+                length += chunk.toString().length
+            });
+            stream.on('end', () => {
+                console.log('chunk end');
+                console.log(length)
+            });
 
-    stream.pipe(res);
+            stream.pipe(res);
+            break;
+
+        case 'POST': // 处理 POST 方法
+            let len = 0;
+            req.on('data', (chunk) => {
+                len += chunk.toString().length;
+                console.log('chunk ', len);
+            });
+            req.on('end', () => {
+                console.log('chunk end');
+                res.end(len+'')
+            });
+            break;
+        default:
+            res.end('method not support')
+    }
 };
 
-var server = http.createServer(function (req, res) {
-    var fileName = path.resolve(__dirname, 'data.txt');
-    streamRead(fileName, res);
+var server = http.createServer((req, res) => {
+    streamRead(req, res);
 });
 
-console.log("listen on 8000");
-server.listen(8000);
+server.listen(8000, () => console.log("listen on 8000"));
