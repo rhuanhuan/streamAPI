cc3b1811f8982ff01d07402f17978d392b83b76e
rhuanhuan <1210098674@qq.com>
2020-03-21 11:42:19 +0800

    stream buffer performance demo

--- stream_buffer.js
+++ stream_buffer.js
@@ -4,3 +4,17 @@ let buffer = Buffer.from(str, 'utf-8');
 console.log(buffer);
 console.log(buffer.toString());
 
+// test buffer performance
+//
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+var server = http.createServer((req, res) => {
+    let filename = path.resolve(__dirname, 'buffer-test.txt');
+    fs.readFile(filename, (err, data) => res.end(data)); // this is buffer
+    // fs.readFile(filename, (err, data) => res.end(data.toString())); // this is string
+});
+
+server.listen(8888, () => console.log('server start on 8888'));
